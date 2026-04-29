866043b034b8ad7038e3cd6d96bd5e7fb6e58d76
q23isline <rowpure@gmail.com>
2026-04-29 17:19:19 +0900

    add: SampleUserのCRUDコントローラーひな型生成 ```bash docker compose exec app php artisan make:controller Api/V1/SampleUserAddController --invokable docker compose exec app php artisan make:controller Api/V1/SampleUserDeleteController --invokable docker compose exec app php artisan make:controller Api/V1/SampleUserGetController --invokable docker compose exec app php artisan make:controller Api/V1/SampleUserListGetController --invokable docker compose exec app php artisan make:controller Api/V1/SampleUserUpdateController --invokable ```

---
+++ app/Http/Controllers/Api/V1/SampleUserAddController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SampleUserAddController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
