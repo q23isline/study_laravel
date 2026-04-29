9c30d7bfe80d1a0530bebc156a225de3241c7750
q23isline <rowpure@gmail.com>
2026-04-29 13:06:47 +0900

    add: SampleUserシード作成 ```bash sudo chmod -R ogu+rw . docker compose exec app php artisan db:seed --class=SampleUserSeeder ```

--- database/seeders/SampleUserSeeder.php
+++ database/seeders/SampleUserSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SampleUserSeeder extends Seeder
 {
@@ -12,6 +12,50 @@ class SampleUserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $now = now();
+        $data = [];
+        for ($i = 0; $i < 52; $i++) {
+            $data[] = [
+                'name' => $this->randomName(),
+                'birth_day' => $this->randomDate(),
+                'height' => rand(140, 200).'.'.rand(0, 9),
+                'gender' => (string) rand(1, 2),
+                'created_at' => $now,
+                'updated_at' => $now,
+            ];
+        }
+
+        DB::table('sample_users')->insert($data);
+    }
+
+    /**
+     * ランダムな名前を生成
+     */
+    private function randomName(): string
+    {
+        $lastNames = ['佐藤', '鈴木', '高橋', '田中', '伊藤', '渡辺', '山本', '中村', '小林', '加藤'];
+        $firstNamesMale = ['太郎', '一郎', '健太', '翔', '直樹', '悠斗', '颯太', '大和', '陽翔', '陸'];
+        $firstNamesFemale = ['花子', '美咲', 'さくら', '結衣', '葵', '玲奈', '七海', '莉子', '楓', '優奈'];
+
+        $lastName = $lastNames[array_rand($lastNames)];
+        if (rand(1, 2) === 1) {
+            $firstName = $firstNamesMale[array_rand($firstNamesMale)];
+        } else {
+            $firstName = $firstNamesFemale[array_rand($firstNamesFemale)];
+        }
+
+        return $lastName.' '.$firstName;
+    }
+
+    /**
+     * ランダムな日付を生成
+     */
+    private function randomDate(): string
+    {
+        $startTimestamp = strtotime('1950-01-01');
+        $endTimestamp = strtotime('2005-12-31');
+        $randomTimestamp = rand($startTimestamp, $endTimestamp);
+
+        return date('Y-m-d', $randomTimestamp);
     }
 }
