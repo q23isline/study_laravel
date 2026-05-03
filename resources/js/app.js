7bcef0405ef1da65fae0727fd96d24ffebfddb7c
q23isline <rowpure@gmail.com>
2026-05-03 16:15:17 +0900

    add: PrimeVue動作確認のためPrimeVue公式クイックスタート移植 https://github.com/primefaces/primevue-examples/tree/9176050f621096ed98617bb84d50263364967939/laravel-quickstart

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,21 @@
-//
+// PrimeVue のクイックスタートをそのまま移植
+// https://github.com/primefaces/primevue-examples/tree/9176050f621096ed98617bb84d50263364967939/laravel-quickstart
+import './bootstrap';
+import { createApp } from 'vue';
+import App from './components/App.vue';
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+
+const app = createApp(App);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            darkModeSelector: '.p-dark'
+        }
+    }
+});
+
+
+app.mount('#app');
---
+++ resources/js/components/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import AppFooter from "./layout/AppFooter.vue";
+import AppTopbar from "./layout/AppTopbar.vue";
+import StatsWidget from "./dashboard/StatsWidget.vue";
+import SalesTrendWidget from "./dashboard/SalesTrendWidget.vue";
+import RecentActivityWidget from "./dashboard/RecentActivityWidget.vue";
+import ProductOverviewWidget from "./dashboard/ProductOverviewWidget.vue";
+</script>
+
+<template>
+    <div
+        class="bg-surface-50 dark:bg-surface-950 min-h-screen p-8 flex flex-col gap-6"
+    >
+        <AppTopbar />
+        <div class="flex flex-col w-full max-w-7xl mx-auto gap-6 flex-1">
+            <StatsWidget />
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <SalesTrendWidget />
+                <RecentActivityWidget />
+            </div>
+            <ProductOverviewWidget />
+        </div>
+        <AppFooter />
+    </div>
+</template>
