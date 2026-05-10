37fcc5925a342a1f60bdb2a7a73207900677c33f
q23isline <rowpure@gmail.com>
2026-05-10 12:30:42 +0900

    add: ViteにInertiaの設定を追加

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue'
+import inertia from '@inertiajs/vite'
 
 export default defineConfig({
     plugins: [
@@ -29,6 +30,7 @@ export default defineConfig({
                 },
             },
         }),
+        inertia(),
     ],
     server: {
         watch: {
